4985bf68c2ae42095a1a496c3f29d759c14d6d79
Cervator <cervator@gmail.com>
2020-08-08 17:03:33 -0500

    chore: Tiny adjustment to go along with https://github.com/MovingBlocks/Terasology/pull/3933

--- src/main/java/org/terasology/corerendering/rendering/dag/nodes/PrePostCompositeNode.java
+++ src/main/java/org/terasology/corerendering/rendering/dag/nodes/PrePostCompositeNode.java
@@ -191,7 +191,7 @@ public class PrePostCompositeNode extends AbstractNode implements PropertyChange
 
         // Shader Parameters
 
-        prePostMaterial.setFloat("viewingDistance", renderingConfig.getViewDistance().getChunkDistance().x * 8.0f, true);
+        prePostMaterial.setFloat("viewingDistance", renderingConfig.getViewDistance().getChunkDistance().x() * 8.0f, true);
         prePostMaterial.setFloat3("cameraParameters", activeCamera.getzNear(), activeCamera.getzFar(), 0.0f, true);
 
         if (localReflectionsAreEnabled) {
