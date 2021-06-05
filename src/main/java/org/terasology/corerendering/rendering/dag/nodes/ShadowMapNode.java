fd5cf3d6dc6264fcd31e1c283376c69cdb725db9
Michael Pollind <mpollind@gmail.com>
2021-06-05 08:49:59 -0700

    refactor(rendering)!: update chunk rendering logic with changes (#59)
    * refactor(rendering)!: update chunk rendering logic with changes
    
    PR: https://github.com/MovingBlocks/Terasology/pull/4735
    
    * chore: clean up states changes in chunkmesh

--- src/main/java/org/terasology/corerendering/rendering/dag/nodes/ShadowMapNode.java
+++ src/main/java/org/terasology/corerendering/rendering/dag/nodes/ShadowMapNode.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.corerendering.rendering.dag.nodes;
 
 import org.joml.Matrix4f;
@@ -164,7 +151,7 @@ public class ShadowMapNode extends ConditionDependentNode implements PropertyCha
             int numberOfChunksThatAreNotReadyYet = 0;
 
             final Vector3f cameraPosition = shadowMapCamera.getPosition();
-            shadowMapMaterial.setMatrix4("projection", shadowMapCamera.getProjectionMatrix(), true);
+            shadowMapMaterial.setMatrix4("projectionMatrix", shadowMapCamera.getProjectionMatrix(), true);
 
             Matrix4f modelViewMatrix = new Matrix4f();
             Matrix4f model = new Matrix4f();
@@ -174,7 +161,7 @@ public class ShadowMapNode extends ConditionDependentNode implements PropertyCha
                 if (chunk.hasMesh()) {
                     model.setTranslation(chunk.getRenderPosition().sub(cameraPosition));
                     modelViewMatrix.set(shadowMapCamera.getViewMatrix()).mul(model);
-                    shadowMapMaterial.setMatrix4("modelView", modelViewMatrix, true);
+                    shadowMapMaterial.setMatrix4("modelViewMatrix", modelViewMatrix, true);
                     numberOfRenderedTriangles += chunk.getMesh().render(OPAQUE);
 
                 } else {
