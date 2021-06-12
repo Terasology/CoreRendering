cea9f9d97d9464563b1ec2832cf7ec8a65709749
Michael Pollind <mpollind@gmail.com>
2021-06-12 15:47:00 -0700

    chore: cleanup

--- src/main/java/org/terasology/corerendering/rendering/dag/nodes/UpdateExposureNode.java
+++ src/main/java/org/terasology/corerendering/rendering/dag/nodes/UpdateExposureNode.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.corerendering.rendering.dag.nodes;
 
 import org.lwjgl.opengl.GL11;
@@ -22,15 +9,13 @@ import org.slf4j.LoggerFactory;
 import org.terasology.engine.config.Config;
 import org.terasology.engine.config.RenderingConfig;
 import org.terasology.engine.context.Context;
-import org.terasology.math.TeraMath;
 import org.terasology.engine.monitoring.PerformanceMonitor;
-import org.terasology.gestalt.naming.Name;
-import org.terasology.nui.properties.Range;
 import org.terasology.engine.rendering.dag.AbstractNode;
 import org.terasology.engine.rendering.opengl.PBO;
 import org.terasology.engine.rendering.opengl.ScreenGrabber;
-
-import java.util.concurrent.atomic.AtomicBoolean;
+import org.terasology.gestalt.naming.Name;
+import org.terasology.math.TeraMath;
+import org.terasology.nui.properties.Range;
 
 /**
  * An instance of this node takes advantage of a downsampled version of the scene,
