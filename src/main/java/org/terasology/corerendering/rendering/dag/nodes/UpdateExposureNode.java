564516d57d22c22af9f1eaa46e7372bc6893de58
jdrueckert <jd.rueckert@googlemail.com>
2023-12-12 11:38:10 +0100

    chore: remove unused imports

--- src/main/java/org/terasology/corerendering/rendering/dag/nodes/UpdateExposureNode.java
+++ src/main/java/org/terasology/corerendering/rendering/dag/nodes/UpdateExposureNode.java
@@ -4,8 +4,6 @@ package org.terasology.corerendering.rendering.dag.nodes;
 
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.terasology.engine.config.Config;
 import org.terasology.engine.config.RenderingConfig;
 import org.terasology.engine.context.Context;
